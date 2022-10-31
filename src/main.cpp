8db01d8fb9d30adeadc71eb29fdb10db15e5d2d6
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-10-31 22:54:14 +0100

    Read in vault

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,29 @@
+#include <fstream>
 #include <iostream>
+#include <iterator>
 #include "GAT/args.hpp"
+
+#ifdef _WIN32
+#define DEFAULT_VAULT_LOCATION "%USERPROFILE%\\.gpwvault"
+#else
+#define DEFAULT_VAULT_LOCATION "~/.gpwvault"
+#endif
+
+constexpr std::pair<std::string_view, bool> source(std::span<std::string_view const> args) noexcept {
+	switch(args.size()) {
+	case 1:
+		return {args[0], {}};
+	case 2:
+		if(args[0][0] == '-')
+			return {args[1], true};
+		else if(args[1][0] == '-')
+			return {args[0], true};
+		[[fallthrough]];
+	case 0:
+	default:
+		return {{}, {}};
+	}
+}
 
 int main(int argc, char ** argv) {
 	struct result {
@@ -18,12 +42,30 @@ int main(int argc, char ** argv) {
 Options:
 	-h, --help      Display this
 	-v, --version   Display the version number
+	-               Use stdin as vault-content
 )";
 		return 0;
 	} else if(r.version) {
 		std::cout << "GPW version 0.1.0\n";
 		return 0;
 	}
-	std::cerr << "gpw: fatal: nothing to do (use -h for help)\n";
-	return 1;
+	auto [item, stdin] = source(a);
+	if(!item.data()) {
+		std::cerr << "gpw: fatal: item not specified correctly (use -h for help)\n";
+		return 1;
+	}
+	std::string_view vault_path{DEFAULT_VAULT_LOCATION};
+	if(auto sep = item.find(':'); sep != item.npos) {
+		vault_path = item.substr(sep + 1);
+		item = item.substr(0, sep);
+	}
+	std::vector<unsigned char> vault;
+	if(stdin) {
+		std::istream_iterator<unsigned char> begin{std::cin}, end;
+		vault.assign(begin, end);
+	} else {
+		std::ifstream file{vault_path.data(), std::ios_base::binary};
+		std::istream_iterator<unsigned char> begin{file}, end;
+		vault.assign(begin, end);
+	}
 }
