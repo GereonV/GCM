d7a9ce16978bf332da82d8049f5eff6e70ad519c
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-11-01 19:12:03 +0100

    Generate a random password

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <random>
 #include "GAT/args.hpp"
 
 #ifdef _WIN32
@@ -9,28 +10,49 @@
 #define DEFAULT_VAULT_LOCATION "$HOME/.gpwvault"
 #endif
 
+constexpr std::size_t to_size(std::string_view sv) noexcept {
+	std::size_t res{};
+	for(auto c : sv)
+		res = res * 10 + c - '0';
+	return res;
+}
+
+static std::string generate_password(std::size_t size, std::string_view allowed = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~") noexcept {
+	static std::mt19937 mt{std::random_device{}()};
+	std::string pass;
+	pass.reserve(size);
+	std::uniform_int_distribution<std::size_t> dist{0, allowed.size() - 1};
+	while(size--)
+		pass += allowed[dist(mt)];
+	return pass;
+}
+
 int main(int argc, char ** argv) try {
 	struct result {
+		std::string_view size;
 		bool help{};
-		bool quiet{};
 		bool version{};
 	};
 	auto [r, a] = gat::args::parse<result, gat::args::all, gat::args::options<
 			gat::args::option<result, 'h', "help", &result::help>,
 			gat::args::option<result, 'v', "version", &result::version>
 		>{}, gat::args::options<
-			gat::args::argoption<result, 0, "", nullptr>
+			gat::args::argoption<result, 'g', "generate", &result::size>
 		>{}>({argv + 1, static_cast<std::size_t>(argc - 1)});
 	if(r.help) {
 		std::cout << R"(Usage: gpw [<options>] [<vault>]
 
 Options:
+	-g, --generate=<size>   Generate a random password (vault is ignored if specified)
 	-h, --help              Display this
 	-v, --version           Display the version number
 )";
 		return 0;
 	} else if(r.version) {
 		std::cout << "GPW version 0.1.0\n";
+		return 0;
+	} else if(r.size.data()) {
+		std::cout << generate_password(to_size(r.size)) << '\n';
 		return 0;
 	}
 	char const * vault_path;
