85f5dcc01c6819b08da3aadcbf87d65b9fe142d0
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-11-02 18:47:26 +0100

    Move IO to seperate header

--- src/gpw.hpp
+++ src/gpw.hpp
@@ -1,13 +1,11 @@
 #ifndef GPW_HPP
 #define GPW_HPP
 
-#include <cstring>
 #include <iosfwd>
 #include <span>
 #include <string>
 #include <string_view>
 #include <utility>
-#include <vector>
 
 namespace gpw {
 
@@ -20,52 +18,31 @@ namespace gpw {
 		}
 	}
 
-	// TODO iterator constructed with password
+	inline std::size_t read_size(std::istream & is) {
+		std::size_t size;
+		is >> size;
+		is.ignore();
+		return size;
+	}
 
-	class vault {
-	public:
-		constexpr ~vault() { clear(); }
-		constexpr bool empty() const noexcept { return _keys.empty(); }
-		constexpr void clear() noexcept {
-			_keys.clear();
-			delete[] std::exchange(_data, nullptr);
-		}
+	inline void write_size(std::ostream & os, std::size_t size) {
+		os << size << '\0';
+	}
 
-		template<typename S>
-		constexpr void emplace_back(S && name, std::string_view data) noexcept {
-			_keys.emplace_back(static_cast<S &&>(name));
-			auto new_size = _size + data.size();
-			if(new_size > _capacity) {
-				auto new_data = new char[_capacity = new_size * 2];
-				std::memcpy(new_data, _data, _size);
-				delete[] std::exchange(_data, new_data);
-			}
-			std::memcpy(_data + _size, data.data(), data.size());
-			_size = new_size;
-		}
+	inline std::pair<std::string, std::string> read(std::istream & is) {
+		std::pair<std::string, std::string> res;
+		std::getline(is, res.first, '\0');
+		auto size = read_size(is);
+		res.second.resize(size);
+		is.read(res.second.data(), size);
+		return res;
+	}
 
-		friend std::istream & operator>>(std::istream & is, vault & v) {
-			v.clear();
-			is >> v._size >> v._capacity;
-			for(v._keys.resize(v._size), is.ignore(); auto & key : v._keys)
-				std::getline(is, key);
-			is.read(v._data = new char[v._size = v._capacity], v._capacity);
-			return is;
-		}
-
-		friend std::ostream & operator<<(std::ostream & os, vault const & v) {
-			os << v._keys.size() << ' ' << v._size << '\n';
-			for(auto && key : v._keys)
-				os << key << '\n';
-			os.write(v._data, v._size);
-			return os;
-		}
-
-	private:
-		std::vector<std::string> _keys;
-		char * _data{};
-		std::size_t _size{}, _capacity{};
-	};
+	inline void write(std::ostream & os, std::string_view name, std::string_view value) {
+		os << name << '\0';
+		write_size(os, value.size());
+		os.write(value.data(), value.size());
+	}
 
 }
 
